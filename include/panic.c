ebca5e107cf6b2ded89ad37bf2e25cf894142b92
amirhnajafiz <najafizadeh21@gmail.com>
2024-12-19 17:56:00 -0500

    remove: old files

--- include/panic.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// panic micor is called to terminate the program with an error message.
-void panic(const char *message) {
-    fprintf(stderr, "Panic: %s\n", message);
-    exit(1);
-}
--- src/handler.h
+++
@@ -1,3 +0,0 @@
-#include <sys/socket.h>
-
-void client_handler(int ns, int namelen, struct sockaddr *client);
