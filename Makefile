a93e2070a6770cf69ae312fd1b0d31484e45b9f5
amirhnajafiz <najafizadeh21@gmail.com>
2024-12-27 13:29:30 -0500

    refactor: code

--- Makefile
+++ Makefile
@@ -1,21 +1,28 @@
+# global variables
 CXX = g++
 CXXFLAGS = -std=c++17 -Wall -Wextra -pedantic
 LDFLAGS = -L./vcpkg_installed/x64-linux/lib -lyaml-cpp
 INCLUDE = -I./vcpkg_installed/x64-linux/include
 
+# project variables
 SRC_DIR = server/src
 CSRC_DIR = client/src
 BUILD_DIR = build
 
+# server variables
 SERVER_SRC = $(SRC_DIR)/server.cpp $(SRC_DIR)/config.cpp $(SRC_DIR)/handler.cpp
 SERVER_OBJ = $(BUILD_DIR)/server.o $(BUILD_DIR)/config.o $(BUILD_DIR)/handler.o
 
+# client variables
 CLIENT_SRC = $(CSRC_DIR)/client.cpp
 CLIENT_OBJ = $(BUILD_DIR)/client.o
 
 .PHONY: all clean
 
+# rules
 all: $(BUILD_DIR)/server $(BUILD_DIR)/client
+server: $(BUILD_DIR)/server
+client: $(BUILD_DIR)/client
 
 $(BUILD_DIR)/server: $(SERVER_OBJ)
 	$(CXX) $(CXXFLAGS) $(INCLUDE) $^ -o $@ $(LDFLAGS)
--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,55 +1,60 @@
 #include "../include/client.h"
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
     // check if the user has provided the correct number of arguments
     if (argc != 2) {
-        std::cerr << "usage: " << argv[0] << " user@host:port" << std::endl;
+        cerr << "usage: " << argv[0] << " user@host:port" << endl;
         return 1;
     }
 
     // parse the user@host:port string
-    std::string userHostPort(argv[1]);
+    string userHostPort(argv[1]);
     size_t atPos = userHostPort.find('@');
     size_t colonPos = userHostPort.find(':');
-    if (atPos == std::string::npos || colonPos == std::string::npos) {
-        std::cerr << "invalid user@host:port string" << std::endl;
+    if (atPos == string::npos || colonPos == string::npos) {
+        cerr << "invalid user@host:port string" << endl;
         return 1;
     }
 
     // extract the user, host, and port
-    std::string user = userHostPort.substr(0, atPos);
-    std::string host = userHostPort.substr(atPos + 1, colonPos - atPos - 1);
-    std::string portStr = userHostPort.substr(colonPos + 1);
+    string user = userHostPort.substr(0, atPos);
+    string host = userHostPort.substr(atPos + 1, colonPos - atPos - 1);
+    string portStr = userHostPort.substr(colonPos + 1);
+    
     int port;
-    std::istringstream(portStr) >> port;
+    istringstream(portStr) >> port;
 
     // create a socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
-        std::cerr << "failed to create socket" << std::endl;
+        cerr << "failed to create socket" << endl;
         return 1;
     }
 
-    // connect to the server
+    // prepare the server address
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     if (inet_pton(AF_INET, host.c_str(), &server.sin_addr) <= 0) {
-        std::cerr << "invalid address" << std::endl;
+        cerr << "invalid address" << endl;
         return 1;
     }
 
     // connect to the server
     if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        std::cerr << "connection failed" << std::endl;
+        cerr << "connection failed" << endl;
         return 1;
     }
 
     // get server response
     char buffer[1024] = {0};
     int valread;
+
+    // read the response from the server
     while ((valread = read(sock, buffer, 1024)) > 0) {
-        std::cout << buffer;
+        cout << buffer;
         memset(buffer, 0, 1024);
     }
 
