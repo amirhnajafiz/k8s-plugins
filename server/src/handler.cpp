6689ba67e3bc1b4e090f417de97f9f4b4013ad02
amirhnajafiz <najafizadeh21@gmail.com>
2024-12-27 13:32:19 -0500

    feat: multiple commands run

--- server/src/handler.cpp
+++ server/src/handler.cpp
@@ -1,59 +1,66 @@
 #include "../include/handler.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstring>
+#include <iostream>
 
 // handle function to execute user commands
 void handle(int client_socket) {
-    // create a pipe
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        return;
-    }
+    char buffer[1024];
+    while (true) {
+        // read command from socket
+        ssize_t bytes_read = read(client_socket, buffer, sizeof(buffer) - 1);
+        if (bytes_read <= 0) {
+            perror("read");
+            break;
+        }
+        buffer[bytes_read] = '\0';
 
-    // fork a child process
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        return;
-    }
-
-    if (pid == 0) {
-        // child process
-        // close the read end of the pipe
-        close(pipefd[0]);
-
-        // redirect stdout to the write end of the pipe
-        dup2(pipefd[1], STDOUT_FILENO);
-
-        // close the write end of the pipe
-        close(pipefd[1]);
-
-        // execute ls command
-        execlp("ls", "ls", NULL);
-
-        // if execlp fails, print an error message
-        perror("execlp");
-        exit(1);
-    } else {
-        // parent process
-        // close the write end of the pipe
-        close(pipefd[1]);
-
-        // read the output of ls command from the read end of the pipe
-        char buffer[1024];
-        ssize_t bytes_read;
-        while ((bytes_read = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
-            // send the output to the client
-            if (send(client_socket, buffer, bytes_read, 0) == -1) {
-                perror("send");
-                break;
-            }
+        // check for exit command
+        if (strcmp(buffer, "exit") == 0) {
+            break;
         }
 
-        // close the read end of the pipe
-        close(pipefd[0]);
+        // create a pipe
+        int pipefd[2];
+        if (pipe(pipefd) == -1) {
+            perror("pipe");
+            return;
+        }
 
-        // wait for the child process to exit
-        int status;
-        waitpid(pid, &status, 0);
+        // fork a child process
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            return;
+        }
+
+        if (pid == 0) {
+            // child process
+            close(pipefd[0]); // close read end of pipe
+            dup2(pipefd[1], STDOUT_FILENO); // redirect stdout to pipe
+            dup2(pipefd[1], STDERR_FILENO); // redirect stderr to pipe
+            close(pipefd[1]);
+
+            // execute command
+            execl("/bin/sh", "sh", "-c", buffer, (char *)NULL);
+            perror("execl");
+            exit(EXIT_FAILURE);
+        } else {
+            // parent process
+            close(pipefd[1]); // close write end of pipe
+
+            // read command output from pipe
+            ssize_t nbytes;
+            while ((nbytes = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0) {
+                buffer[nbytes] = '\0';
+                write(client_socket, buffer, nbytes);
+            }
+            close(pipefd[0]);
+
+            // wait for child process to finish
+            waitpid(pid, NULL, 0);
+        }
     }
+    close(client_socket);
 }
