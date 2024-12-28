0287f33c94a14b4c1c49f13655e17999f23d4030
amirhnajafiz <najafizadeh21@gmail.com>
2024-12-28 14:18:56 -0500

    bug: in pipe read

--- server/src/handler.cpp
+++ server/src/handler.cpp
@@ -35,9 +35,6 @@ void handle(int client_socket) {
         return;
     }
 
-    // Set the read end of the pipe to non-blocking
-    fcntl(fileno(shell_read), F_SETFL, O_NONBLOCK);
-
     while (true) {
         // Read command from the client socket
         ssize_t nbytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
@@ -58,42 +55,20 @@ void handle(int client_socket) {
 
         cout << "Command executed" << endl;
 
-        // Set up the file descriptor set
-        fd_set read_fds;
-        FD_ZERO(&read_fds);
-        FD_SET(fileno(shell_read), &read_fds);
-
-        struct timeval timeout;
-        timeout.tv_sec = 10; // Increase timeout to 10 seconds
-        timeout.tv_usec = 0;
-
-        cout << "Waiting for command output..." << endl;
-
-        int retval = select(fileno(shell_read) + 1, &read_fds, NULL, NULL, &timeout);
-
-        if (retval == -1) {
-            std::cerr << "Error in select: " << strerror(errno) << std::endl;
-        } else if (retval == 0) {
-            std::cerr << "Timeout occurred! No data available." << std::endl;
-        } else {
-            cout << "Data available, reading from pipe..." << endl;
-            // Read the command output from the pipe
-            while ((nbytes = read(fileno(shell_read), buffer, sizeof(buffer) - 1)) > 0) {
-                buffer[nbytes] = '\0'; // Null-terminate the buffer
-                ssize_t obytes = send(client_socket, buffer, nbytes, 0); // Send the output back to the client
-                if (obytes < 0) {
-                    std::cerr << "Error sending command output: " << strerror(errno) << std::endl;
-                    break;
-                } else if (obytes == 0) {
-                    std::cerr << "Connection closed by client" << std::endl;
-                    break;
-                }
-                cout << "Command output: " << buffer << endl;
+        // Read the command output from the pipe
+        while (fgets(buffer, sizeof(buffer), shell_read) != NULL) {
+            ssize_t obytes = send(client_socket, buffer, strlen(buffer), 0); // Send the output back to the client
+            if (obytes < 0) {
+                std::cerr << "Error sending command output: " << strerror(errno) << std::endl;
+                break;
+            } else if (obytes == 0) {
+                std::cerr << "Connection closed by client" << std::endl;
+                break;
             }
+        }
 
-            if (nbytes < 0) {
-                std::cerr << "Error reading from pipe: " << strerror(errno) << std::endl;
-            }
+        if (ferror(shell_read)) {
+            std::cerr << "Error reading from pipe: " << strerror(errno) << std::endl;
         }
 
         cout << "Command output sent" << endl;
